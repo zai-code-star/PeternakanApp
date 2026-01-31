4974d3bea4fed16e7b58c98c5d484ba1a8c00f7c
zai-code-star <zainulmuhajir5@gmail.com>
2026-01-31 17:08:33 +0700

    Peternakan App Ui Only

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.peternakanapps"
+        applicationId = "com.project.peternakanapps"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
